b5a0a84f112af8309b8d88f37bb7231c07fc6d6f
MikheichevAA <andrey@MacBook-Pro-Andrey.local>
2025-11-06 09:25:51 +0300

    Добавлена картинка

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,11 @@
+import boilers from "../assets/1.jpg";
+
 const Gallery = () => {
   return (
     <section>
       <h2>Архитектурная визуализация</h2>
       <img src="/fahwerk.jpg" alt="Дом" />
+      <img src={boilers} alt="Котельная" />
     </section>
   );
 };
