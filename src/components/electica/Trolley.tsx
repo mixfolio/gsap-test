70f95a79caba216d1ce0dfa5ac5395fd5c8f8f64
MikheichevAA <andrey@MacBook-Pro-Andrey.local>
2025-11-18 22:24:01 +0300

    Создан спрайт выкатной тележки

--- src/components/electica/Trolley.tsx
+++ src/components/electica/Trolley.tsx
@@ -2,21 +2,14 @@ import type { FC } from "react";
 import "./trolley.css";
 
 type TrolleyProps = {
-  color: string;
-  state: "a" | "b" | "c" | "d" | "e" | "f";
+  color?: string;
+  state?: "var1" | "b" | "c" | "d" | "e" | "f";
 } & React.SVGProps<SVGSVGElement>;
-export const Trolley: FC<TrolleyProps> = ({ color, state, ...props }) => {
+export const Trolley: FC<TrolleyProps> = ({ ...props }) => {
   return (
     <>
-      <svg>
-        <use
-          xlinkHref="./trolley-sprite.svg#trolley-a"
-          //   className="test1"
-          stroke={color}
-          strokeWidth={4}
-          //   fill={"blue"}
-          fill="none"
-        />
+      <svg {...props}>
+        <use xlinkHref="./trolley-sprite.svg#trolley-var4" className="var2" />
       </svg>
     </>
   );
